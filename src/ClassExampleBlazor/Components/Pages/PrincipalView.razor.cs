dffb92f5f9f42b1aea8df11a9d431cf9d70d6761
Felipe Mora <lmora@growthaccelerationpartners.com>
2026-04-21 19:34:03 -0600

    add mocks

---
+++ src/ClassExampleBlazor/Components/Pages/PrincipalView.razor.cs
@@ -0,0 +1,27 @@
+﻿using ClassController;
+using Microsoft.AspNetCore.Components;
+
+namespace ClassExampleBlazor.Components.Pages
+{
+    /// <summary>
+    /// C# code behind for PrincipalView.razor.
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Components.ComponentBase" />
+    public partial class PrincipalView : ComponentBase
+    {
+        [Inject]
+        private CalculatorController CalculatorControllerService { get; set; } = new CalculatorController();
+
+        private int Value1 { get; set; } = 0;
+        private int Value2 { get; set; } = 0;
+        private string Result { get; set; } = string.Empty;
+
+        private CalculatorController calculatorController = new CalculatorController();
+
+        private void Sum()
+        {
+            // this.Result = calculatorController.Sum(this.Value1, this.Value2).ToString();
+            this.Result = CalculatorControllerService!.Sum(this.Value1, this.Value2).ToString();
+        }
+    }
+}
---
+++ test/ClassControllerTest/LoginControllerTest.cs
@@ -0,0 +1,128 @@
+﻿using ClassController;
+using ClassController.Abstractions;
+using ClassExampleModel;
+using Moq;
+
+namespace ClassControllerTest
+{
+    /// <summary>
+    /// Test for <see cref="LoginController"/>.
+    /// </summary>
+    [TestClass]
+    public class LoginControllerTest
+    {
+        /// <summary>
+        /// Logins the should be successful.
+        /// </summary>
+        [TestMethod]
+        public void LoginShouldBeSuccessful()
+        {
+            // Arrange
+            var dataHandlerMock = new Mock<IDataHandler<User>>();
+            var users = new List<User>
+            {
+                new ("user","123"),
+                new ("user2","123"),
+                new ("user3","123"),
+            };
+
+            dataHandlerMock.Setup(x => x.LoadData()).Returns(users);
+            var userController = new UserController(dataHandlerMock.Object);
+            var loginController = new LoginController(userController);
+
+            // Act
+            var result = loginController.Login("user", "123");
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// Login with wrong password should fail.
+        /// </summary>
+        [TestMethod]
+        public void LoginWithWrongPasswordShouldFail()
+        {
+            // Arrange
+            var dataHandlerMock = new Mock<IDataHandler<User>>();
+            var users = new List<User>
+            {
+                new ("user","123"),
+                new ("user2","123"),
+                new ("user3","123"),
+            };
+
+            dataHandlerMock.Setup(x => x.LoadData()).Returns(users);
+            var userController = new UserController(dataHandlerMock.Object);
+            var loginController = new LoginController(userController);
+
+            // Act
+            var result = loginController.Login("user", "1234");
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        /// <summary>
+        /// Login without users should not crash.
+        /// </summary>
+        [TestMethod]
+        public void LoginWithoutUsersShouldNotCrash()
+        {
+            // Arrange
+            var dataHandlerMock = new Mock<IDataHandler<User>>();
+            var users = new List<User>();
+
+            dataHandlerMock.Setup(x => x.LoadData()).Returns(users);
+            var userController = new UserController(dataHandlerMock.Object);
+            var loginController = new LoginController(userController);
+
+            // Act
+            var result = loginController.Login("user", "1234");
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        /// <summary>
+        /// Tests the login with user controller mock.
+        /// Note: this is a unit test, the others could be seen as integration test.
+        /// </summary>
+        [TestMethod]
+        public void TestLoginWithUserControllerMock()
+        {
+            // Arrange
+            var userControllerMock = new Mock<IUserController>();
+            userControllerMock.Setup(x => x.Login("Gerardo", "SomePassword")).Returns(true);
+
+            var loginController = new LoginController(userControllerMock.Object);
+
+            // Act
+            var result = loginController.Login("Gerardo", "SomePassword");
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// Tests the login with user controller mock with any args.
+        /// Note: this is a unit test, the others could be seen as integration test.
+        /// </summary>
+        [TestMethod]
+        public void TestLoginWithUserControllerMockWithAnyArgs()
+        {
+            // Arrange
+            var userControllerMock = new Mock<IUserController>();
+            userControllerMock.Setup(x => x.Login(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+
+            var loginController = new LoginController(userControllerMock.Object);
+
+            // Act
+            // var result = loginController.Login("Gerardo", "SomePassword");
+            var result = loginController.Login("Gerardo", "123");
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
